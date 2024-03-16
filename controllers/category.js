5b2c6b6553768bd03b39545ef8a2a4dd9c61a69a
Shubham  Saurav <shubhamsinha186@gmail.com>
2024-03-16 23:42:11 +0530

    CRUD Operations Done For Category but untested

--- controllers/category.js
+++ controllers/category.js
@@ -4,30 +4,64 @@ const Category = require('../models/Category')
 // @desc        : Get all categories
 // @access      : Public
 exports.getCategories = async (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        categories: []
-    })
+    try {
+        const categories = await Category.find()
+        res.status(200).json({
+            success: true,
+            categories: categories
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: '500 Server Error!'
+        })
+    }
+    
 }
 
 // @route       : GET /api/v1/categories/:id
 // @desc        : Get single category
 // @access      : Public
-exports.getCategories = async (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        category: {}
-    })
+exports.getCategory = async (req, res, next) => {
+
+    try {
+        const category = await Category.findById(req.params.id)
+        if(!category) {
+            res.status(404).json({
+                success: false,
+                message: "Resource Not Found"
+            })
+        }
+        res.status(200).json({
+            success: true,
+            category: category
+        }) 
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: '500 Server Error!'
+        })
+    }
+
+    
 }
 
 // @route       : POST /api/v1/categories
 // @desc        : Create category
 // @access      : Private
 exports.createCategory = async (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        category: {}
-    })
+    try {
+        const category = await Category.create(req.body)
+        res.status(200).json({
+            success: true,
+            category
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: '500 Server Error!'
+        })
+    }
 }
 
 // @route       : PATCH /api/v1/categories/:id
--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const dotenv = require('dotenv')
 
 dotenv.config({path: './config/config.env'})
 
+const categoryRoute = require('./routes/category')
+
 const app = express()
+
+app.use(express.json())
+
+app.use('/api/v1/categories', categoryRoute)
 
 app.all('*', (req, res) => {
     res.status(200).json({
